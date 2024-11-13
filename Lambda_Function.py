4a67f42d11407ccc26a0cdc057aee433305de9a2
Sai Ram Reddy Obul Reddy <sairamreddyobulreddy@gmail.com>
2024-11-12 19:20:13 -0500

    Add files via upload

---
+++ Lambda_Function.py
@@ -0,0 +1,45 @@
+import boto3
+import requests
+import json
+from datetime import datetime
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('WeatherData')
+
+def lambda_handler(event, context):
+    city = "London"
+    api_key = "d44f359691598ea422843d959ffdb89d"  # Replace with your actual OpenWeatherMap API key
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url).json()
+
+    # Process the data
+    # Convert temperature from Kelvin to Celsius and set precision to two decimal places
+    temperature = Decimal(response["main"]["temp"] - 273.15).quantize(Decimal('0.01'))
+    weather = response["weather"][0]["description"]
+
+    # Store to DynamoDB
+    date = datetime.now().strftime("%Y-%m-%d")
+    time = datetime.now().strftime("%H:%M:%S")
+
+    table.put_item(
+        Item={
+            "date": date,
+            "time": time,
+            "temperature": temperature,
+            "description": weather,
+        }
+    )
+
+    # Prepare the response data
+    weather_data = {
+        "date": date,
+        "time": time,
+        "temperature": float(temperature),  # Convert Decimal to float for JSON serialization
+        "description": weather
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(weather_data)  # JSON-encode the weather data
+    }
